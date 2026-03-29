dbed89c1877373b3f19b32dec53ae64e964404d3
CasseShimada <c382304209@gmail.com>
2026-03-29 19:33:21 +0800

    Remove line width vertices from the giant block safe outline path.

--- src/client/java/twilightforest/event/ClientEvents.java
+++ src/client/java/twilightforest/event/ClientEvents.java
@@ -419,12 +419,10 @@ public class ClientEvents {
 				zNormal /= normalLength;
 				consumer.addVertex(pose, (float) (x1 + xyz.x()), (float) (y1 + xyz.y()), (float) (z1 + xyz.z()))
 					.setColor(0.0F, 0.0F, 0.0F, 0.45F)
-					.setNormal(pose, xNormal, yNormal, zNormal)
-					.setLineWidth(1.0F);
+					.setNormal(pose, xNormal, yNormal, zNormal);
 				consumer.addVertex(pose, (float) (x2 + xyz.x()), (float) (y2 + xyz.y()), (float) (z2 + xyz.z()))
 					.setColor(0.0F, 0.0F, 0.0F, 0.45F)
-					.setNormal(pose, xNormal, yNormal, zNormal)
-					.setLineWidth(1.0F);
+					.setNormal(pose, xNormal, yNormal, zNormal);
 			}
 		);
 	}
