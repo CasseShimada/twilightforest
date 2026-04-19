71c99e8a6a9b3f7d4bce44c704812910f80c5c2d
CasseShimada <c382304209@gmail.com>
2026-04-19 18:37:28 +0800

    Fix moonworm and trophy item rendering

--- src/client/java/twilightforest/renderer/block/MoonwormRenderer.java
+++ src/client/java/twilightforest/renderer/block/MoonwormRenderer.java
@@ -6,7 +6,6 @@ import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
-import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.Direction;
@@ -56,7 +55,12 @@ public class MoonwormRenderer implements BlockEntityRenderer<MoonwormBlockEntity
 		stack.mulPose(Axis.YN.rotationDegrees(yaw));
 
 		model.setupAnim(wiggleRotation, delay);
-		collector.submitModel(model, null, stack, RenderTypes.entityCutout(TEXTURE), light, OverlayTexture.NO_OVERLAY, -1, null);
+		collector.submitCustomGeometry(stack, model.renderType(TEXTURE), (pose, consumer) -> {
+			PoseStack modelStack = new PoseStack();
+			modelStack.last().pose().set(pose.pose());
+			modelStack.last().normal().set(pose.normal());
+			model.root().render(modelStack, consumer, light, OverlayTexture.NO_OVERLAY, -1);
+		});
 
 		stack.popPose();
 	}
