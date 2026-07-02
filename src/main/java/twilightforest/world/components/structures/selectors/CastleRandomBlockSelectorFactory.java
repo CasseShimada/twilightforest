ea9a2ec141d4a486123513d8734c2b7b5881fed5
CasseShimada <c382304209@gmail.com>
2026-07-03 02:48:44 +0800

    refactor: expose worn castle brick variants directly

--- src/main/java/twilightforest/world/components/structures/selectors/CastleRandomBlockSelectorFactory.java
+++ src/main/java/twilightforest/world/components/structures/selectors/CastleRandomBlockSelectorFactory.java
@@ -10,8 +10,8 @@ public class CastleRandomBlockSelectorFactory {
 	public SimpleRandomBlockSelector make() {
 		return new SimpleRandomBlockSelector(
 			List.of(
-				Pair.of(TFBlocks.WORN_CASTLE_BRICK.get().defaultBlockState(), 0.1F),
-				Pair.of(TFBlocks.CRACKED_CASTLE_BRICK.get().defaultBlockState(), 0.1F),
+				Pair.of(TFBlocks.WORN_CASTLE_BRICK.defaultBlockState(), 0.1F),
+				Pair.of(TFBlocks.CRACKED_CASTLE_BRICK.defaultBlockState(), 0.1F),
 				Pair.of(TFBlocks.CASTLE_BRICK.defaultBlockState(), 0.8F)
 			)
 		);
