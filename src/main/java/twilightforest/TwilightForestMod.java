7460be59d87431e6269be6b8ec10a6eadb169749
CasseShimada <c382304209@gmail.com>
2026-07-05 07:39:13 +0800

    refactor: expose wood fences directly

--- src/main/java/twilightforest/TwilightForestMod.java
+++ src/main/java/twilightforest/TwilightForestMod.java
@@ -239,7 +239,7 @@ public final class TwilightForestMod implements ModInitializer {
 		flammables.add(TFBlocks.TWILIGHT_OAK_PLANKS.get(), 5, 20);
 		flammables.add(TFBlocks.TWILIGHT_OAK_SLAB, 5, 20);
 		flammables.add(TFBlocks.TWILIGHT_OAK_STAIRS.get(), 5, 20);
-		flammables.add(TFBlocks.TWILIGHT_OAK_FENCE.get(), 5, 20);
+		flammables.add(TFBlocks.TWILIGHT_OAK_FENCE, 5, 20);
 		flammables.add(TFBlocks.TWILIGHT_OAK_GATE.get(), 5, 20);
 
 		flammables.add(TFBlocks.CANOPY_LOG.get(), 5, 5);
@@ -253,7 +253,7 @@ public final class TwilightForestMod implements ModInitializer {
 		flammables.add(TFBlocks.CANOPY_PLANKS.get(), 5, 20);
 		flammables.add(TFBlocks.CANOPY_SLAB, 5, 20);
 		flammables.add(TFBlocks.CANOPY_STAIRS.get(), 5, 20);
-		flammables.add(TFBlocks.CANOPY_FENCE.get(), 5, 20);
+		flammables.add(TFBlocks.CANOPY_FENCE, 5, 20);
 		flammables.add(TFBlocks.CANOPY_GATE.get(), 5, 20);
 		flammables.add(TFBlocks.CANOPY_BOOKSHELF, 5, 20);
 
--- src/main/java/twilightforest/world/components/structures/lichtowerrevamp/LichTowerUtil.java
+++ src/main/java/twilightforest/world/components/structures/lichtowerrevamp/LichTowerUtil.java
@@ -70,7 +70,7 @@ public class LichTowerUtil {
 		Blocks.COBBLESTONE_WALL,
 		Blocks.MOSSY_COBBLESTONE_WALL,
 		TFBlocks.WROUGHT_IRON_FENCE,
-		TFBlocks.CANOPY_FENCE.value(),
+		TFBlocks.CANOPY_FENCE,
 		TFBlocks.TWISTED_STONE_PILLAR
 	));
 
