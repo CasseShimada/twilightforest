2198e06b5c577d2a153df83ebe647d643503c56b
CasseShimada <c382304209@gmail.com>
2026-07-12 12:26:15 +0800

    refactor: restore legacy murals from world context

--- src/main/java/twilightforest/world/components/structures/finalcastle/FinalCastleMuralComponent.java
+++ src/main/java/twilightforest/world/components/structures/finalcastle/FinalCastleMuralComponent.java
@@ -11,16 +11,16 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceSerializationContext;
+import org.jetbrains.annotations.Nullable;
 import twilightforest.init.TFBlocks;
 import twilightforest.init.TFStructurePieceTypes;
 import twilightforest.mixin.accessor.StructurePieceFieldsAccessor;
-import twilightforest.util.WorldUtil;
 import twilightforest.world.components.structures.TFStructureComponentOld;
 
 
 public class FinalCastleMuralComponent extends TFStructureComponentOld {
 
-	private final FinalCastleMural mural;
+	private volatile @Nullable FinalCastleMural mural;
 
 	public FinalCastleMuralComponent(StructurePieceSerializationContext ctx, CompoundTag nbt) {
 		super(TFStructurePieceTypes.TFFCMur, nbt);
@@ -28,7 +28,7 @@ public class FinalCastleMuralComponent extends TFStructureComponentOld {
 		if (nbt.contains("muralBytes")) {
 			this.mural = new FinalCastleMural(nbt);
 		} else {
-			this.mural = new FinalCastleMural(this.boundingBox, ((StructurePieceFieldsAccessor) this).twilightforest$getOrientation(), WorldUtil.getOverworldSeed() + (this.boundingBox.minX() * 321534781L) ^ (this.boundingBox.minZ() * 756839L));
+			this.mural = null;
 		}
 	}
 
@@ -44,23 +44,34 @@ public class FinalCastleMuralComponent extends TFStructureComponentOld {
 	protected void addAdditionalSaveData(StructurePieceSerializationContext ctx, CompoundTag tagCompound) {
 		super.addAdditionalSaveData(ctx, tagCompound);
 
-		this.mural.writeIntoTag(tagCompound);
+		FinalCastleMural mural = this.mural;
+		if (mural != null) {
+			mural.writeIntoTag(tagCompound);
+		}
 	}
 
 	@Override
 	public void postProcess(WorldGenLevel world, StructureManager manager, ChunkGenerator generator, RandomSource rand, BoundingBox sbb, ChunkPos chunkPosIn, BlockPos blockPos) {
+		FinalCastleMural mural = this.getOrCreateMural(world.getSeed());
 
 		final BlockState castleMagic = TFBlocks.YELLOW_CASTLE_RUNE_BRICK.defaultBlockState();
 
 		// copy mural to world
-		for (int x = 0; x < this.mural.width; x++) {
-			for (int y = 0; y < this.mural.height; y++) {
-				if (this.mural.get(x, y) > 0) {
+		for (int x = 0; x < mural.width; x++) {
+			for (int y = 0; y < mural.height; y++) {
+				if (mural.get(x, y) > 0) {
 					this.placeBlock(world, castleMagic, 0, y, x, sbb);
 				} else {
 					//this.setBlockState(world, TFBlocks.forceField, 0, 0, y, x, sbb);
 				}
 			}
 		}
+	}
+
+	private synchronized FinalCastleMural getOrCreateMural(long worldSeed) {
+		if (this.mural == null) {
+			this.mural = new FinalCastleMural(this.boundingBox, ((StructurePieceFieldsAccessor) this).twilightforest$getOrientation(), worldSeed + (this.boundingBox.minX() * 321534781L) ^ (this.boundingBox.minZ() * 756839L));
+		}
+		return this.mural;
 	}
 }
