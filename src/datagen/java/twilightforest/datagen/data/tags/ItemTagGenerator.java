ae05168528bdaeca0601d0c806323bc2829e01f0
CasseShimada <c382304209@gmail.com>
2026-07-13 16:05:40 +0800

    data: generate vanilla wood item tags

--- src/datagen/java/twilightforest/datagen/data/tags/ItemTagGenerator.java
+++ src/datagen/java/twilightforest/datagen/data/tags/ItemTagGenerator.java
@@ -1,8 +1,14 @@
 package twilightforest.datagen.data.tags;
 
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
+import twilightforest.init.TFBlocks;
 import twilightforest.tags.TFBlockTags;
 import twilightforest.tags.TFItemTags;
 
@@ -27,5 +33,35 @@ public final class ItemTagGenerator extends FabricTagsProvider.ItemTagsProvider 
 		this.copy(TFBlockTags.TOWERWOOD, TFItemTags.TOWERWOOD);
 		this.copy(TFBlockTags.BANISTERS, TFItemTags.BANISTERS);
 		this.copy(TFBlockTags.DRYING_RACKS, TFItemTags.DRYING_RACKS);
+
+		this.copy(BlockTags.WOODEN_TRAPDOORS, ItemTags.WOODEN_TRAPDOORS);
+		this.copy(BlockTags.WOODEN_STAIRS, ItemTags.WOODEN_STAIRS);
+		this.copy(BlockTags.WOODEN_SLABS, ItemTags.WOODEN_SLABS);
+		this.copy(BlockTags.WOODEN_PRESSURE_PLATES, ItemTags.WOODEN_PRESSURE_PLATES);
+		this.copy(BlockTags.WOODEN_FENCES, ItemTags.WOODEN_FENCES);
+		this.copy(BlockTags.WOODEN_DOORS, ItemTags.WOODEN_DOORS);
+		this.copy(BlockTags.WOODEN_BUTTONS, ItemTags.WOODEN_BUTTONS);
+		this.copy(BlockTags.FENCE_GATES, ItemTags.FENCE_GATES);
+		this.copy(BlockTags.PLANKS, ItemTags.PLANKS);
+		this.copy(BlockTags.LOGS, ItemTags.LOGS);
+		this.builder(ItemTags.LEAVES).add(
+			key(TFBlocks.RAINBOW_OAK_LEAVES.asItem()),
+			key(TFBlocks.TWILIGHT_OAK_LEAVES.asItem()),
+			key(TFBlocks.CANOPY_LEAVES.asItem()),
+			key(TFBlocks.MANGROVE_LEAVES.asItem()),
+			key(TFBlocks.DARK_LEAVES.asItem()),
+			key(TFBlocks.TIME_LEAVES.asItem()),
+			key(TFBlocks.TRANSFORMATION_LEAVES.asItem()),
+			key(TFBlocks.MINING_LEAVES.asItem()),
+			key(TFBlocks.SORTING_LEAVES.asItem()),
+			key(TFBlocks.THORN_LEAVES.asItem()),
+			key(TFBlocks.BEANSTALK_LEAVES.asItem())
+		);
+		this.copy(BlockTags.STANDING_SIGNS, ItemTags.SIGNS);
+		this.copy(BlockTags.CEILING_HANGING_SIGNS, ItemTags.HANGING_SIGNS);
+	}
+
+	private static ResourceKey<Item> key(Item item) {
+		return BuiltInRegistries.ITEM.getResourceKey(item).orElseThrow();
 	}
 }
