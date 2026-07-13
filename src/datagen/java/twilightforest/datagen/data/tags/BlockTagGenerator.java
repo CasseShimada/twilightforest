0328156daf748683747fc71eb6a9e5c891a464e3
CasseShimada <c382304209@gmail.com>
2026-07-13 13:46:54 +0800

    data: migrate soil lily pad and portal tags

--- src/datagen/java/twilightforest/datagen/data/tags/BlockTagGenerator.java
+++ src/datagen/java/twilightforest/datagen/data/tags/BlockTagGenerator.java
@@ -2,7 +2,9 @@ package twilightforest.datagen.data.tags;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.Block;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import twilightforest.init.TFBlocks;
@@ -17,6 +19,21 @@ public final class BlockTagGenerator extends FabricTagsProvider.BlockTagsProvide
 	@Override
 	protected void addTags(HolderLookup.Provider registries) {
 		this.builder(BlockTags.ENCHANTMENT_POWER_PROVIDER)
-			.add(BuiltInRegistries.BLOCK.getResourceKey(TFBlocks.CANOPY_BOOKSHELF).orElseThrow());
+			.add(key(TFBlocks.CANOPY_BOOKSHELF));
+
+		this.builder(BlockTags.DIRT).add(key(TFBlocks.UBEROUS_SOIL));
+		this.builder(BlockTags.OVERRIDES_MUSHROOM_LIGHT_REQUIREMENT).add(key(TFBlocks.UBEROUS_SOIL));
+		this.builder(BlockTags.SUPPORTS_SMALL_DRIPLEAF).add(key(TFBlocks.UBEROUS_SOIL));
+
+		this.builder(BlockTags.FROG_PREFER_JUMP_TO).add(key(TFBlocks.HUGE_LILY_PAD));
+		this.builder(BlockTags.INSIDE_STEP_SOUND_BLOCKS).add(key(TFBlocks.HUGE_LILY_PAD));
+		this.builder(BlockTags.SWORD_EFFICIENT).add(key(TFBlocks.HUGE_LILY_PAD));
+
+		this.builder(BlockTags.INVALID_SPAWN_INSIDE).add(key(TFBlocks.TWILIGHT_PORTAL));
+		this.builder(BlockTags.PORTALS).add(key(TFBlocks.TWILIGHT_PORTAL));
+	}
+
+	private static ResourceKey<Block> key(Block block) {
+		return BuiltInRegistries.BLOCK.getResourceKey(block).orElseThrow();
 	}
 }
