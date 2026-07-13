88fc1d50c47fc95b4ef007829c39cf2cebaadad1
CasseShimada <c382304209@gmail.com>
2026-07-13 19:55:59 +0800

    data: generate banner patterns

--- src/datagen/java/twilightforest/datagen/TwilightForestDataGenerator.java
+++ src/datagen/java/twilightforest/datagen/TwilightForestDataGenerator.java
@@ -6,6 +6,7 @@ import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import twilightforest.TFRegistries;
 import twilightforest.TwilightForestMod;
+import twilightforest.datagen.data.registries.BannerPatternGenerator;
 import twilightforest.datagen.data.tags.BannerPatternTagGenerator;
 import twilightforest.datagen.data.tags.BiomeTagGenerator;
 import twilightforest.datagen.data.tags.BlockEntityTypeTagGenerator;
@@ -32,6 +33,7 @@ public final class TwilightForestDataGenerator implements DataGeneratorEntrypoin
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator dataGenerator) {
 		FabricDataGenerator.Pack pack = dataGenerator.createPack();
+		pack.addProvider(BannerPatternGenerator::new);
 		BlockTagGenerator blockTags = pack.addProvider(BlockTagGenerator::new);
 		pack.addProvider((output, registries) -> new ItemTagGenerator(output, registries, blockTags));
 		pack.addProvider(BannerPatternTagGenerator::new);
