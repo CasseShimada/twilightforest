1ecd1c80eae08c49611185671b62345ef3179024
CasseShimada <c382304209@gmail.com>
2026-07-13 17:33:35 +0800

    data: add Fabric entity type tag provider

--- src/datagen/java/twilightforest/datagen/TwilightForestDataGenerator.java
+++ src/datagen/java/twilightforest/datagen/TwilightForestDataGenerator.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import twilightforest.TwilightForestMod;
 import twilightforest.datagen.data.tags.BlockTagGenerator;
+import twilightforest.datagen.data.tags.EntityTypeTagGenerator;
 import twilightforest.datagen.data.tags.ItemTagGenerator;
 
 public final class TwilightForestDataGenerator implements DataGeneratorEntrypoint {
@@ -12,6 +13,7 @@ public final class TwilightForestDataGenerator implements DataGeneratorEntrypoin
 		FabricDataGenerator.Pack pack = dataGenerator.createPack();
 		BlockTagGenerator blockTags = pack.addProvider(BlockTagGenerator::new);
 		pack.addProvider((output, registries) -> new ItemTagGenerator(output, registries, blockTags));
+		pack.addProvider(EntityTypeTagGenerator::new);
 	}
 
 	@Override
---
+++ src/datagen/java/twilightforest/datagen/data/tags/EntityTypeTagGenerator.java
@@ -0,0 +1,43 @@
+package twilightforest.datagen.data.tags;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.EntityTypes;
+import twilightforest.init.TFEntities;
+import twilightforest.tags.TFEntityTypeTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public final class EntityTypeTagGenerator extends FabricTagsProvider.EntityTypeTagsProvider {
+	public EntityTypeTagGenerator(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+		super(output, registries);
+	}
+
+	@Override
+	protected void addTags(HolderLookup.Provider registries) {
+		this.builder(TFEntityTypeTags.DONT_KILL_BUGS).add(key(TFEntities.MOONWORM_SHOT));
+		this.builder(TFEntityTypeTags.LICH_DEFLECTS_PHASE_2)
+			.add(key(TFEntities.WAND_BOLT))
+			.add(key(TFEntities.LICH_BOLT))
+			.add(key(TFEntities.LICH_BOMB));
+		this.builder(TFEntityTypeTags.RIDES_OBSTRUCT_SNATCHING)
+			.add(key(TFEntities.PINCH_BEETLE))
+			.add(key(TFEntities.YETI))
+			.add(key(TFEntities.ALPHA_YETI));
+		this.builder(TFEntityTypeTags.SORTABLE_ENTITIES)
+			.add(key(EntityTypes.CHEST_MINECART))
+			.add(key(EntityTypes.HOPPER_MINECART))
+			.add(key(EntityTypes.LLAMA))
+			.add(key(EntityTypes.TRADER_LLAMA))
+			.add(key(EntityTypes.DONKEY))
+			.add(key(EntityTypes.MULE));
+	}
+
+	private static ResourceKey<EntityType<?>> key(EntityType<?> entityType) {
+		return BuiltInRegistries.ENTITY_TYPE.getResourceKey(entityType).orElseThrow();
+	}
+}
